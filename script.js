bdf22bf24057c089be4733c3ce39454b3e99bedc
Tanishq510 <52479073+Tanishq510@users.noreply.github.com>
2022-11-27 19:54:37 +0530

    Resolved dynamic issue issue

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
-
-let string = "";
+let string = '';
 let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>
-{
-  button.addEventListener('click', (e)=> 
-  {
-    if(e.target.innerHTML == '='){
-      console.log(string)
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      console.log(string);
       string = eval(string);
       document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
+    } else if (e.target.innerHTML == 'C') {
+      string = '';
       document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'DEL'){
+    } else if (e.target.innerHTML == 'DEL') {
       let inputValue = document.getElementsByClassName('input')[0].value;
       string = inputValue.slice(0, -1);
       document.querySelector('input').value = string;
+    } else if (e.target.innerHTML == '√') {
+      let freshinput = document.querySelector('input').value;
+      string = Math.sqrt(freshinput);
+      document.querySelector('input').value = string;
+    } else {
+      console.log(e.target);
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
     }
-    else if(e.target.innerHTML == '√'){
+  });
+});
 
-     let freshinput = document.querySelector('input').value;
-     string = Math.sqrt(freshinput);
-     document.querySelector('input').value = string;
-
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-    }  
-  })
-   
-})
+function changeImage() {
+  let randomNumber = Math.round(Math.random() * 10);
+  let image = `https://picsum.photos/900/600?random=${randomNumber}`;
+  document.body.style.backgroundImage = "url('" + image + "')";
+  console.log('image changed');
+}
